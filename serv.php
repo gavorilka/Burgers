454f6314b4f11c9ae7771a23c50301431a066408
gavorilka <59140477+gavorilka@users.noreply.github.com>
2022-08-25 16:40:19 +0300

    add empty

--- serv.php
+++ serv.php
@@ -1,8 +1,8 @@
 <?php
     header('Content-Type: application/json; charset=utf-8');
     
-    if($_POST['name'] && $_POST['phone']){
-        $data = [$_POST['name'],$_POST['phone'],'res'=>'good'];
+    if(!empty($_POST['name']) && !empty($_POST['phone'])){
+        $data = ['name'=> $_POST['name'], 'phone'=> $_POST['phone'],'res'=>'good'];
         echo json_encode($data);
     } else {
         echo json_encode(['res'=>'falid']);
